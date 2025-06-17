9226a405d0a52c20d2d54944067756ca34cfeb46
amandeep <deepaman0556@gmail.com>
2025-06-17 21:05:45 +0530

    feat(middleware):add multer for image uploading handling

---
+++ src/middlewares/multer.middlewares.js
@@ -0,0 +1,17 @@
+import * as path from "node:path";
+import multer from "multer";
+
+const storage = multer.diskStorage({
+  destination: function (req, file, cb) {
+    cb(null, "../../public/temp");
+  },
+  filename: function (req, file, cb) {
+    // todo
+    const uniqueSuffix = Date.now() + "-" + Math.round(Math.random() * 1e9);
+    cb(null, file.originalname + "-" + path.extname(file.originalname));
+  },
+});
+
+export const upload = multer({
+  storage,
+});
