f8d5eb5095876f6e1eb4b63ef4559302c10b5193
amandeep <deepaman0556@gmail.com>
2025-06-17 21:07:30 +0530

    feat(storage):add cloudinary support for remote media storage

---
+++ src/utils/cloudinary.js
@@ -0,0 +1,28 @@
+import * as fs from "node:fs";
+import { v2 as cloudinary } from "cloudinary";
+
+// configure cloudinary
+
+cloudinary.config({
+  cloud_name: process.env.CLOUDINARY_CLOUD_NAME,
+  api_key: process.env.CLOUDINARY_API_KEY,
+  api_secret: process.env.CLOUDINARY_API_SECRET,
+});
+
+const uploadOnCloudinary = async (localFilePath) => {
+  try {
+    if (!localFilePath) return null;
+    const response = await cloudinary.uploader.upload(localFilePath, {
+      resource_type: "auto",
+    });
+    console.log("File uploaded in cloudinary. File src: ", response.url);
+    // once the file is uploaded, we would like to delete it from our servers
+    fs.unlinkSync(localFilePath);
+    return response;
+  } catch (error) {
+    fs.unlinkSync(localFilePath);
+    return null;
+  }
+};
+
+export { uploadOnCloudinary };
